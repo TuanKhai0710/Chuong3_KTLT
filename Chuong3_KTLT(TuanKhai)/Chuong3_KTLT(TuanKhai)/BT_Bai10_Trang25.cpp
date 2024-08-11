1aafd5ecaf3c6d5d2e86ae9a6a2d1dbedcddf11f
TuanKhai0710 <lvtk07102003@gmail.com>
2024-08-11 23:59:20 +0700

    Chuong3_Bai10/25

---
+++ Chuong3_KTLT(TuanKhai)/Chuong3_KTLT(TuanKhai)/BT_Bai10_Trang25.cpp
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+#define MAX_CHARS 256
+
+char findMostFrequentChar(const char *str) {
+	int count[MAX_CHARS] = { 0 };
+	int maxCount = 0;
+	char result;
+
+	// Đếm số lần xuất hiện của mỗi ký tự
+	for (int i = 0; str[i] != '\0'; i++) {
+		count[(unsigned char)str[i]]++;
+		if (count[(unsigned char)str[i]] > maxCount) {
+			maxCount = count[(unsigned char)str[i]];
+			result = str[i];
+		}
+	}
+
+	return result;
+}
+
+int main() {
+	char str[1000];
+
+	printf("Nhap chuoi: ");
+	fgets(str, sizeof(str), stdin);
+
+	// Loại bỏ ký tự newline ở cuối chuỗi
+	str[strcspn(str, "\n")] = '\0';
+
+	char mostFrequentChar = findMostFrequentChar(str);
+
+	printf("Ky tu xuat hien nhieu nhat la: '%c'\n", mostFrequentChar);
+	printf("Nhan Enter de ket thuc chuong trinh...\n");
+	getchar(); // Đọc ký tự Enter còn lại từ buffer
+	getchar(); // Chờ người dùng nhấn Enter 
+	return 0;
+}
